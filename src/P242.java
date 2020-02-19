d053a4ff2f23a4ea2ca777790e8308c7feba2d9f
zhangmingao <zhangmingao@91jinrong.com>
2020-02-19 10:26:06 +0800

    p242 相同字母异序词

--- src/P242.java
+++ src/P242.java
@@ -1,6 +1,34 @@
+/**
+ * 242. Valid Anagram
+ * 相同字母异序词
+ * 有效的字母移位词
+ */
 public class P242 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(isAnagram("anagram", "nagaram") ? "true" : "false");
     }
+
+
+    public static boolean isAnagram(String s, String t) {
+        int word[] = new int[30];
+        int word2[] = new int[30];
+        char[] charlistS = s.toCharArray();
+        char[] charlistT = t.toCharArray();
+        for(int i = 0; i < charlistS.length; i++) {
+            word[charlistS[i] - 'a']++;
+        }
+
+        for(int i = 0; i < charlistT.length; i++) {
+            word2[charlistT[i] - 'a']++;
+        }
+
+        for(int i = 0; i < 30; i++) {
+            if(word[i] != word2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
