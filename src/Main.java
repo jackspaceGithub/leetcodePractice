bcf1ee4c9adc6cac0c02ba3000ed4b8a09a03558
zhangmingao <zhangmingao@91jinrong.com>
2020-04-14 14:17:32 +0800

    P876 寻找链表中间的点，（快慢指针法可以省去数组和计数变量）

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        P25 p25 = new P25();
-        p25.main();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.removeIf(e -> e >= 2);
+        System.out.println("Hello World!");
     }
 }
